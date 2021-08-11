4a2902a3d03a341b7011ba8f5280ce47c1f50584
jazzg62 <714508719@qq.com>
2021-08-11 14:11:46 +0800

    pages场景适应

--- src/app.config.js
+++ src/app.config.js
@@ -1,23 +1,43 @@
-export default {
-  pages: [
+let pages = [];
+switch (process.env.TARO_ENV) {
+  case "weapp":
+    pages = [
+      "pages/index/index",
+      "pages/pay/pay",
+      "pages/phone/phone",
+      "pages/share/share",
+      "pages/welcome/welcome",
 
-    'pages/index/index',
-    'pages/pay/pay',
-    'pages/phone/phone',
-    'pages/share/share',
-    'pages/welcome/welcome',
+      "pages/scan/scan",
+      "pages/discount/discount",
+      "pages/password/password",
+      "pages/success/success"
+    ];
+    break;
+  case "alipay":
+    pages = [
+      "pages/index/index",
+      // "pages/pay/pay",
+      // "pages/phone/phone",
+      // "pages/share/share",
+      "pages/welcome/welcome",
 
-
-    'pages/scan/scan',
-    'pages/discount/discount',
-    'pages/password/password',
-    'pages/success/success',
-
-  ],
-  window: {
-    backgroundTextStyle: 'light',
-    navigationBarBackgroundColor: '#fff',
-    navigationBarTitleText: '企联商务',
-    navigationBarTextStyle: 'black'
-  }
+      "pages/scan/scan",
+      "pages/discount/discount",
+      "pages/password/password",
+      "pages/success/success"
+    ];
+    break;
+  default:
+    break;
 }
+
+export default {
+  pages,
+  window: {
+    backgroundTextStyle: "light",
+    navigationBarBackgroundColor: "#fff",
+    navigationBarTitleText: "企联商务",
+    navigationBarTextStyle: "black"
+  }
+};
